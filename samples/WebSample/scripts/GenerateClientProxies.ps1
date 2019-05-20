127ab7109c2b539f88632ac17aef501289834481
Mark Zuber <mzuber@microsoft.com>
2019-05-20 11:43:29 -0700

    fix generateclientproxies path

--- samples/WebSample/scripts/GenerateClientProxies.ps1
+++ samples/WebSample/scripts/GenerateClientProxies.ps1
@@ -1,9 +1,9 @@
 $scriptPath = Split-Path $script:MyInvocation.MyCommand.Path
 $projectRoot = (get-item $scriptPath).parent.FullName
 
-docker run --rm -v ${projectRoot}:/local swaggerapi/swagger-codegen-cli-v3:3.0.8 generate -i /local/src/IdentityWeb/wwwroot/api-docs/v1/swagger.json -l csharp -o /local/out/csharp --additional-properties packageName=IdentityApi.Client
+docker run --rm -v ${projectRoot}:/local swaggerapi/swagger-codegen-cli-v3:3.0.8 generate -i /local/samples/websample/IdentityWeb/wwwroot/api-docs/v1/swagger.json -l csharp -o /local/out/csharp --additional-properties packageName=IdentityApi.Client
 
-xcopy /s /e /Y $projectRoot\out\csharp\src\IdentityApi.Client\Api\*.* $projectRoot\src\IdentityApi.Client\Api\
+xcopy /s /e /Y $projectRoot\out\csharp\src\IdentityApi.Client\Api\*.* $projectRoot\samples\websample\IdentityApi.Client\Api\
 # xcopy /s /e /Y $projectRoot\out\csharp\src\IdentityApi.Client\Client\*.* $projectRoot\src\IdentityApi.Client\Client\
 
 function Get-ScriptDirectory {
